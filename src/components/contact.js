95af46d2202cbfd48cefad9ea1b3cbbc8408a977
jhaameehu <jamiuazeez49@gmail.com>
2020-04-07 23:40:08 -0700

    created a portfolio app with create react app and added files to homepage

---
+++ src/components/contact.js
@@ -0,0 +1,12 @@
+import react, {Component} from 'react';
+
+class Component extends Component {
+    render (){
+        return (
+            <div>
+                <h1>Contact Page</h1>
+            </div>
+        )
+    }
+}
+export default Contact;
---
+++ src/components/projects.js
@@ -0,0 +1,12 @@
+import react, {Component} from 'react';
+
+class Project extends Component {
+    render (){
+        return (
+            <div>
+                <h1>Project Page</h1>
+            </div>
+        )
+    }
+}
+export default Project;
