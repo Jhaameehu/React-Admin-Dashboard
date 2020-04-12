0f98df09ecab7f65ec658e650b79e11d73142b99
jhaameehu <jamiuazeez49@gmail.com>
2020-04-11 18:52:31 -0700

    updated my landing page

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -8,10 +8,24 @@ class Landing extends Component {
                 <Grid className="landing-grid">
                     <cell col={12}>
                         <img 
-                        src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
+                        src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shareicon.net%2Fmale-business-man-costume-avatar-office-user-103157&psig=AOvVaw2dNFc60QxMXRgUzyakjEfo&ust=1586560920717000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLjS8tGp3egCFQAAAAAdAAAAABAD"
                         alt="avatar"
                         className="avatar-img"
                         />
+                        <div className="banner-text">
+                            <h1> Full Stack Web Developer</h1>
+
+                            <hr/>
+
+                            <p>HTML/CSS |  CSS Grid |  Bootstrap | Javascript | React | Dart | Flutter  </p>
+                            <div className="social-links">
+                                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-linkedin-square" aria-hidden="true" />
+
+                                </a>
+
+                            </div>
+                        </div>
                         
                     </cell>
                 </Grid>
